cb6b829f3d1713394550c8d2ecc698802970aaca
Vivekchaurasia03 <vivekc03022001@gmail.com>
2024-02-11 23:52:53 +0530

    getrequest CLI tool Check-in

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main() -> Result<()> {
+    // We are making a request to the mentioned URL and storing the response in the res varibale.
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+
+    let mut body = String::new();
+
+    /* Read all bytes until EOF in this source, appending them to buf.
+    If successful, this function returns the number of bytes which were read and appended to buf */
+    res.read_to_string(&mut body)?;
+
+    // Printing the contents we would like to see on the terminal.
+    println!("Status: {}", res.status());
+    println!("Headers: \n{:#?}", res.headers());
+    println!("Body: \n{}", body);
+    Ok(())
+}
